a905533cf4682c080bd0f525ee5868c2d595c637
Adriel-cante <65051923+Adriel-cante@users.noreply.github.com>
2020-06-04 22:04:33 -0500

    CursoSpring
    Tarea Programacion cuarto semestre

--- src/main/java/com/bytecode/core/controllers/ControllerBasic.java
+++ src/main/java/com/bytecode/core/controllers/ControllerBasic.java
@@ -3,11 +3,14 @@ package com.bytecode.core.controllers;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.bytecode.core.configuration.Paginas;
@@ -30,17 +33,32 @@ public class ControllerBasic {
 		return post;
 	}
 
-	@GetMapping(path= {"/post","/"})
+	@GetMapping(path= {"/posts","/"})
 	public String saludar(Model model) {
 		model.addAttribute("Posts", this.getPosts());
 		
 		return "index";
 	}
 	
-	@GetMapping(path="/public")
+	/**@GetMapping(path="/public")
 	public ModelAndView post() {
 		ModelAndView modelAndView = new ModelAndView(Paginas.HOME);
 		modelAndView.addObject("Posts", this.getPosts());
+		return modelAndView;
+	}**/
+	
+	@GetMapping(path = {"/post", "/post/p/{post}"})
+	public ModelAndView getPostIndividual(
+			/**@RequestParam(defaultValue = "1", name = "id", required = true)**/ @PathVariable(required = true, name = "post" ) int id
+			) {
+		ModelAndView modelAndView = new ModelAndView(Paginas.POST);
+		
+		List<Post> postFiltrado = this.getPosts()
+								.stream()
+								.filter( (p) -> {
+									return p.getId() == id;
+								}).collect(Collectors.toList());
+		modelAndView.addObject("post", postFiltrado.get(0));
 		return modelAndView;
 	}
 }
